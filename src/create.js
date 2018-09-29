f8fbfd178b702ed1e3d52956615cc423190daaf6
Thorsten Lorenz <thlorenz@gmx.de>
2018-09-29 16:19:16 -0600

    bo: stopping due to API challenges
    - v3 changed the API entirely and isn't yet fully feature complete it seems
    - also the animations don't look smooth at all with just one ball bouncing around

--- src/create.js
+++ src/create.js
@@ -1,8 +1,16 @@
 export default ctx => function create() {
   const ball = this.physics.add.image(50, 50, 'ball')
-  ctx.set('sprite:ball', ball)
+  ctx.set('physics:ball', ball)
   ball
     .setVelocity(350, 350)
     .setBounce(1, 1)
     .setCollideWorldBounds(true)
+
+  // API changed drastically from v2 to v3 which is super annoying, so
+  // hardcoding dimensions for now
+  const { width, height } = { width: 1000, height: 700 } 
+  ctx.set(
+      'sprite:paddle'
+    , this.add.sprite(width * 0.5, height - 5, 'paddle')
+  )
 }
